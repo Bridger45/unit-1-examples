fee9eedffe38e440e25990a216e24141d3c81870
Bridger45 <42648470+Bridger45@users.noreply.github.com>
2018-09-07 09:27:04 -0600

    adding unit 1 examples

---
+++ unit 1 examples.py
@@ -0,0 +1,38 @@
+##print("this is a string")
+##name = "bridger"
+##print(type(name))
+##fact = "i like to play msuic and dance "
+##message = "my name is " + name + " and " + fact
+##print(message)
+
+##num = 8
+##num2 = 14
+##
+##
+##print(num+num2)
+##
+##answer = num + num2
+##print(answer)
+
+word1 = input("enter an adjective")
+word2 = input("enter annother adjective")
+word3 = input("enter a noun")
+word4 = input("enter another noun")
+word5 = input("enter yet another noun")
+word6 = input("enter an adjective")
+word7 = input("enter a verb")
+print("Frosty the Snowman was a",
+      word1,
+      word2,
+      "soul. With a corncob",
+      word3,
+      "and a button nose and two eyes made out of",
+      word4,
+      ".",
+      "Frosty the snow man is a fairy tale they say. He is made of",
+      word5,
+      "but the chldren know how he came to life one day. There must have been some magic in that",
+      word6,
+      "hat they found. For when they placed it on his head he baegan to",
+      word7,
+      "around.")
